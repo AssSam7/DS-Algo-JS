04b550ed660db878f0b54531bc8494d8f4bd3b58
Aslam Mohammed <mohammadaslamparvez7@gmail.com>
2020-08-19 20:58:39 +0530

    Adding insert() to linked list

--- 03 Linked Lists/Impls/LinkedListImpl.js
+++ 03 Linked Lists/Impls/LinkedListImpl.js
@@ -46,11 +46,58 @@ class LinkedList {
     this.length++;
     return this;
   }
+
+  // Insert the value at a specified index
+  insert(index, value) {
+    // Check arguments
+    if (index >= this.length) {
+      return this.append(value);
+    }
+    if (index == 0) {
+      return this.prepend(value);
+    }
+
+    const newNode = new Node(value);
+    // To get the leading node i.e. the node before current inserting element
+    const leaderNode = this.traverseToIndex(index - 1);
+    const holdingPointer = leaderNode.next;
+    leaderNode.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this.traverseList();
+  }
+
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter != index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+
+  // Traversal through the linked list and print in the form of an array
+  traverseList() {
+    let linkedListArr = [];
+    let currentNode = this.head;
+    for (let i = 0; i < this.length; i++) {
+      linkedListArr.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    // while (currentNode !== null) {
+    //   linkedListArr.push(currentNode.value);
+    //   currentNode = currentNode.next;
+    // }
+    return linkedListArr;
+  }
 }
 
 // Linked List: 10 --> 5 --> 16
 const myLinkedList = new LinkedList(10);
-console.log(myLinkedList.append(5));
-console.log(myLinkedList.append(16));
-console.log(myLinkedList.prepend(2));
-// console.log(myLinkedList);
+myLinkedList.append(5);
+myLinkedList.append(16);
+myLinkedList.prepend(2);
+console.log(myLinkedList.insert(2, 15));
+console.log(myLinkedList.insert(0, 99));
+// console.log(myLinkedList.traverseList());
